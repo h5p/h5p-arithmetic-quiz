7f1d72f806392bf2db7ec464b6cca5ac14ca9765
Thomas Horn Sivertsen <thomass@cerpus.com>
2018-03-27 16:20:35 +0200

    Added/modified calculation of max score

--- presave.js
+++ presave.js
@@ -1,18 +1,20 @@
 var H5PPresave = H5PPresave || {};
 
 H5PPresave['H5P.ArithmeticQuiz'] = function (content, finished) {
-    var score = 0;
+  var presave = H5PEditor.Presave;
+  var score = 0;
 
-    if( typeof content === 'object' && content.hasOwnProperty('maxQuestions')){
-        score = content.maxQuestions;
-    }
+  if (isContentValid()) {
+    score = content.maxQuestions;
+  }
 
-    if( isNaN(score) || score < 0){
-        throw {
-            name: 'InvalidMaxScore Error',
-            message: "Could not calculate the max score for this content. The max score is assumed to be 0. Contact your administrator if this isn’t correct."
-        };
-    }
+  presave.validateScore(score);
 
+  if (finished) {
     finished({maxScore: score});
+  }
+
+  function isContentValid() {
+    return presave.checkNestedRequirements(content, 'content.maxQuestions');
+  }
 };
